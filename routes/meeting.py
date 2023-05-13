4cb277a0626fdd6bb0e86d132dbca7ce90948926
glondonot <glondonot@unal.edu.co>
2023-05-13 11:45:47 -0500

    .

--- routes/meeting.py
+++ routes/meeting.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, make_response, request, redirect, flash
 from config.server_conf import db
-from controller.Meeting import Meeting
+from Controller.Meeting import Meeting
 from datetime import datetime
 
 meetings = Blueprint("meetings", __name__)
